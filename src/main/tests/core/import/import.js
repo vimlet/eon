954169754db7f7fe387c7bb585ba3f338d08bd82
richardvimlet <richardillesk@gmail.com>
2018-04-11 11:25:51 +0200

    Fixed issue on life-cycle test for firefox

--- src/main/tests/core/import/import.js
+++ src/main/tests/core/import/import.js
@@ -6,29 +6,28 @@ vcomet.tests.core = vcomet.tests.core || {
 
 vcomet.onReady(function () {
 
-    console.log(JSON.stringify(Object.keys(vcomet.imports.config), null, 2));
-
-    var obj = {};
+    var imports = [];
     var imported = Object.keys(vcomet.imports.config);
     var dependencies;
 
     for (var i = 0; i < imported.length; i++) {
         
-        obj[imported[i]] = {};
-        obj[imported[i]].dependencies = [];
+        imports.push({});
+        imports[i].name = imported[i];
+        imports[i].dependencies = [];
 
         if (vcomet.imports.config[imported[i]].dependencies) {
 
             dependencies = vcomet.imports.config[imported[i]].dependencies;
             
             for (var j = 0; j < dependencies.length; j++) {
-                obj[imported[i]].dependencies.push(dependencies[j].replace(".html", "").toLowerCase());
+                imports[i].dependencies.push(dependencies[j].replace(".html", "").toLowerCase());
             }
 
         }
         
     }
 
-    console.log('obj', JSON.stringify(obj, null, 2));
+    console.log('imports', JSON.stringify(imports, null, 2));
 
 })
