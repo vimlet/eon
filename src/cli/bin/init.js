f949c8c3d46139fec6be6b92ca4507c618ff0013
lethevimlet <lethevimlet@mail.com>
2018-05-23 17:13:44 +0200

    Improved cli init mode

--- src/cli/bin/init.js
+++ src/cli/bin/init.js
@@ -4,7 +4,7 @@ var path = require("path");
 var os = require("os");
 var fs = require("fs-extra");
 var url = require("url");
-var readlineSync = require('readline-sync');
+var readlineSync = require("readline-sync");
 var Sync = require("sync");
 var install = require("./install");
 
@@ -15,49 +15,82 @@ var projectPath;
 var cwd = process.cwd();
 
 module.exports = function (result) {
-    console.log("\nWelcome to vComet");
 
     projectPath = result.path ? result.path : cwd;
 
     Sync(function () {
-        try {
-            templateName = handleVersion.sync(null);
-            downloadAndExtractTemplate.sync(null, releaseURL, templateName, projectPath);
 
-            // Call to install vcomet
-            install.sync(null, { install: true });
+        console.log("\nWelcome to vComet\n");
 
-            console.log('\x1b[96m', '\nTo start the server, type "npm start"');
-            console.log("\x1b[0m"); // Reset color + newLine
+        var downloadTemplate = readlineSync.question("Initialize project with a template? [yes] ");
+        if (downloadTemplate.trim() == "" || downloadTemplate.toLowerCase().indexOf("y") == 0) {
+            try {
+                templateName = handleVersion.sync(null);
+                downloadAndExtractTemplate.sync(null, releaseURL, templateName, projectPath);
 
-        } catch (error) {
-            console.error("\x1b[91m", "\nError: " + error);
-            console.error("\x1b[0m"); // Reset color + newLine
+                // Call to install vcomet
+                install.sync(null, { install: true });
+
+                console.log('\x1b[96m', '\nTo start the server, type "npm start"');
+                console.log("\x1b[0m"); // Reset color + newLine
+
+            } catch (error) {
+                console.error("\x1b[91m", "\nError: " + error);
+                console.error("\x1b[0m"); // Reset color + newLine
+            }
+        } else {
+
+            var vcometJsonObject = {
+                vcomet: "latest",
+                path: "vcomet",
+                ignore: [
+                    "custom"
+                ]
+            };
+
+            var vcometVersion = readlineSync.question("\nvComet version? [latest] ");
+            if(vcometVersion.trim() != "" && vcometVersion.toLowerCase() != "latest") {
+                vcometJsonObject.vcomet = vcometVersion;
+            }
+
+            var vcometPath = readlineSync.question("\nvComet path? [vcomet] ");
+            if (vcometPath.trim() == "") {
+                vcometPath = "vcomet"
+            }
+            vcometJsonObject.path = vcometPath;
+
+            // Write file
+            fs.writeFileSync("vcomet.json", JSON.stringify(vcometJsonObject, null, 2));
+
+             // Call to install vcomet
+             install.sync(null, { install: true });
         }
-    })
+    });
+
+
 
 }
 
 function handleVersion(cb) {
     var url = "https://api.github.com/repos/vimlet/VimletComet-Examples/releases/tags/vcomet-init";
-    
-        var xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = function () {
-            if (this.readyState == 4) {
-                if (this.status == 200) {
-                    var responseObject = JSON.parse(xhttp.responseText);
-                    var templateName = responseObject.assets[0].name;
 
-                    cb(null, templateName);
-                } else {
-                    cb("Request status " + this.status);
-                }
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4) {
+            if (this.status == 200) {
+                var responseObject = JSON.parse(xhttp.responseText);
+                var templateName = responseObject.assets[0].name;
+
+                cb(null, templateName);
+            } else {
+                cb("Request status " + this.status);
             }
-        };
-    
-        xhttp.open("GET", url, true);
-        xhttp.setRequestHeader("User-Agent", "vimlet");
-        xhttp.send();
+        }
+    };
+
+    xhttp.open("GET", url, true);
+    xhttp.setRequestHeader("User-Agent", "vimlet");
+    xhttp.send();
 }
 
 function downloadAndExtractTemplate(releaseURL, templateName, projectPath, cb) {
