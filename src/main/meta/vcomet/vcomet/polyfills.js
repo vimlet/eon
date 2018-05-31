58e4e80194618cb9bba18975f0a279cac409759f
sergiovimlet <sergio@lightware.es>
2018-05-31 14:03:06 +0200

    Added classList remove proxy

--- src/main/meta/vcomet/vcomet/polyfills.js
+++ src/main/meta/vcomet/vcomet/polyfills.js
@@ -117,6 +117,23 @@ if (vcomet.needClassListAddPolyfill()) {
 
     };
   })(DOMTokenList.prototype.add);
+  
+  (function (proxied) {
+
+    DOMTokenList.prototype.remove = function () {
+
+      if (arguments.length > 1) {
+
+        for (var i = 0; i < arguments.length; i++) {
+          proxied.apply(this, [arguments[i]]);
+        }
+
+      } else {
+        return proxied.apply(this, arguments);
+      }
+
+    };
+  })(DOMTokenList.prototype.remove);
 
 }
 
