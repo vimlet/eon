a8af7931afe501cdeaa8426338fecd8dc21c716b
sergiovimlet <sergio@lightware.es>
2018-06-05 17:32:17 +0200

    Implementing theme tester elements tree navigation

--- src/main/example/ui/themetester/js/util.js
+++ src/main/example/ui/themetester/js/util.js
@@ -7,20 +7,37 @@ function setUpSearch (selector) {
 }
 
 function loadVCometExamples () {
-    var anchor, activePanel, anchorPosition;
+    var anchor, activePanel;
     // Configure tree
     refs.tree.vcometScroll.thickness = "10";
     // Load vComet element example
     refs.tree.onNodeSelected(function(node) {
         activePanel = refs.view.getActivePanel();
         anchor = activePanel.querySelector("[state=" + node.id + "]");
-        //
-        if (anchor) {
-            anchorPosition = anchor.getOffsetPosition();
-            // console.log('anchorPosition', anchorPosition);
-            // if(anchorPosition)
-            // Scroll to specific element section
-            activePanel.vcometScroll.scrollTop = [anchor.getOffsetPosition(), true];
+
+        if(node.type == "file" && activePanel.name == node._refs.parentNode.id) {
+            console.log('node.type', node.type);
+            //
+            anchor = activePanel.querySelector("[state=" + node.id + "]");
+            if(anchor) {
+                // Scroll to the specific element section
+                activePanel.vcometScroll.scrollTop = [anchor.getOffsetPosition(), true];
+            } else {
+                refs.view.swapToPanel(node._refs.parentNode.id);
+                activePanel = refs.view.getActivePanel()
+                // Update scroll
+                activePanel.vcometScroll.update();
+            }
+        } else if (node.type == "file" && activePanel.name != node._refs.parentNode.id){
+            anchor = activePanel.querySelector("[state=" + node.id + "]");
+            if(anchor) {
+                refs.view.swapToPanel(node._refs.parentNode.id);
+                activePanel = refs.view.getActivePanel();
+                // Scroll to the specific element section
+                activePanel.vcometScroll.scrollTop = [anchor.getOffsetPosition(), true];
+                // Update scroll
+                activePanel.vcometScroll.update();
+            }
         } else {
             refs.view.swapToPanel(node.id);
             // Update scroll
