a1953c3177f711258ea0e04e6440268f3700ee1e
sergiovimlet <sergio@lightware.es>
2018-06-05 17:05:35 +0200

    Added scrollTo scroll limit

--- src/main/example/ui/themetester/js/util.js
+++ src/main/example/ui/themetester/js/util.js
@@ -7,16 +7,19 @@ function setUpSearch (selector) {
 }
 
 function loadVCometExamples () {
-    var anchor, activePanel;
+    var anchor, activePanel, anchorPosition;
     // Configure tree
     refs.tree.vcometScroll.thickness = "10";
     // Load vComet element example
     refs.tree.onNodeSelected(function(node) {
         activePanel = refs.view.getActivePanel();
-        anchor = activePanel.querySelector("[state=" + node.id + "]")
+        anchor = activePanel.querySelector("[state=" + node.id + "]");
         //
         if (anchor) {
-            // Scroll to
+            anchorPosition = anchor.getOffsetPosition();
+            // console.log('anchorPosition', anchorPosition);
+            // if(anchorPosition)
+            // Scroll to specific element section
             activePanel.vcometScroll.scrollTop = [anchor.getOffsetPosition(), true];
         } else {
             refs.view.swapToPanel(node.id);
