5c39cc240a9a546b70224ed8c23795ca1f9e7a75
sergiovimlet <sergio@lightware.es>
2018-06-06 10:23:50 +0200

    Fixed scroll to 0 bug

--- src/main/example/ui/themetester/js/util.js
+++ src/main/example/ui/themetester/js/util.js
@@ -12,51 +12,19 @@ function loadVCometExamples () {
     refs.tree.vcometScroll.thickness = "10";
     // Load vComet element example
     refs.tree.onNodeSelected(function(node) {
-        activePanel = refs.view.getActivePanel();
-        anchor = activePanel.querySelector("[state=" + node.id + "]");
-        console.log('node clicked: ', node._refs.parentNode);
-
         // Go to group file
         groupId = node._refs.parentNode.tagName == "VC-TREENODE" ? node._refs.parentNode.id : node.id; 
-
         refs.view.swapToPanel(groupId);
-
-        // if(anchor) {
-        //     console.log('file group: ', node._refs.parentNode.id);
-        //     refs.view.swapToPanel(node._refs.parentNode.id);
-        // } else {
-        // }
-
+        // Get active panel
+        activePanel = refs.view.getActivePanel();
+        // Get node related anchor
+        anchor = activePanel.querySelector("[state=" + node.id + "]");
+        if(anchor) {
+            console.log('anchor', anchor, anchor.getOffsetPosition());
+           // Scroll to the specific element section
+           activePanel.vcometScroll.scrollTop = [anchor.getOffsetPosition(), true];
+        }
         // Update scroll
         activePanel.vcometScroll.update();
-
-        // if(node.type == "file" && activePanel.name == node._refs.parentNode.id) {
-        //     console.log('node.type', node.type);
-        //     //
-        //     anchor = activePanel.querySelector("[state=" + node.id + "]");
-        //     if(anchor) {
-        //         // Scroll to the specific element section
-        //         activePanel.vcometScroll.scrollTop = [anchor.getOffsetPosition(), true];
-        //     } else {
-        //         refs.view.swapToPanel(node._refs.parentNode.id);
-        //         activePanel = refs.view.getActivePanel()
-        //         // Update scroll
-        //         activePanel.vcometScroll.update();
-        //     }
-        // } else if (node.type == "file" && activePanel.name != node._refs.parentNode.id){
-        //     anchor = activePanel.querySelector("[state=" + node.id + "]");
-        //     if(anchor) {
-        //         refs.view.swapToPanel(node._refs.parentNode.id);
-        //         activePanel = refs.view.getActivePanel();
-        //         // Scroll to the specific element section
-        //         activePanel.vcometScroll.scrollTop = [anchor.getOffsetPosition(), true];
-        //         // Update scroll
-        //         activePanel.vcometScroll.update();
-        //     }
-        // } else {
-        //     refs.view.swapToPanel(node.id);
-        //     // Update scroll
-        //     activePanel.vcometScroll.update();
-        // }
     })
 }
