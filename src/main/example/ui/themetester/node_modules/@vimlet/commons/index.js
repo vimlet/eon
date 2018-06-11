23e6d03b5b8ad011056eca07e1ddfbc7b6edd438
sergiovimlet <sergio@lightware.es>
2018-06-11 15:21:31 +0200

    remove themetester unnecessary modules

--- src/main/example/ui/themetester/node_modules/@vimlet/commons/index.js
+++
@@ -1,6 +0,0 @@
-exports.os = require("./lib/os.js");
-exports.run = require("./lib/run.js");
-exports.request = require("./lib/request.js");
-exports.compress = require("./lib/compress.js");
-exports.progress = require("./lib/progress.js");
-exports.io = require("./lib/io.js");
