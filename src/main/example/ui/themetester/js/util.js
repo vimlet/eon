9f4998836080b1a395fa248438bc6647962a96ae
tamara <tamara@mail.com>
2018-10-04 13:19:02 +0200

    Fixed tree and panel scroll bug

--- src/main/example/ui/themetester/js/util.js
+++ src/main/example/ui/themetester/js/util.js
@@ -1,4 +1,4 @@
-function setUpSearch (selector) {
+function setUpSearch(selector) {
     // Search listener
     document.querySelector(selector).onSearch(function (filters) {
         // Loop filtering targets
@@ -6,30 +6,31 @@ function setUpSearch (selector) {
     });
 }
 
-function loadVCometExamples () {
+function loadVCometExamples() {
     var anchor, activePanel, groupId;
     // Configure tree
-    refs.tree.vcometScroll.thickness = "10";
+    var treeScroll = refs.tree.querySelector("vc-scroll");
+    treeScroll.thickness = "10";
+
     // Load vComet element example
-    refs.tree.onNodeSelected(function(node) {
+    refs.tree.onNodeSelected(function (node) {
         // Go to group file
-        groupId = node._refs.parentNode.tagName == "VC-TREENODE" ? node._refs.parentNode.id : node.id; 
+        groupId = node._refs.parentNode.tagName == "VC-TREENODE" ? node._refs.parentNode.id : node.id;
         refs.view.swapToPanel(groupId);
         // Get active panel
         activePanel = refs.view.getActivePanel();
+        var panelScroll = activePanel.$1("vc-scroll");
         // Get node related anchor
         anchor = activePanel.querySelector("[state=" + node.id + "]");
-        if(anchor) {
-           // Scroll to the specific element section
-           activePanel.vcometScroll.scrollTop = [anchor.getOffsetPosition(), true];
+        if (anchor) {
+            // Scroll to the specific element section
+            panelScroll.scrollTop = [anchor.getOffsetPosition(), true];
         }
-        // Update scroll
-        activePanel.vcometScroll.update();
     })
 }
 
 function toggleMenu(forceAction) {
-    
+
     var drawer = document.querySelector(".tTreeContainer");
     if ((drawer._misc.displayed && !forceAction) || forceAction == "close") {
         drawer.close();
