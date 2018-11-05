6e83ceb5746b8f53cfa049c091489f22db5ebe06
sergiovimlet <sergio@lightware.es>
2018-11-05 16:15:15 +0100

    Implemented eon offline support example

--- eon-framework/src/copy/webapp/eon/data/eon-cache/service-worker.js
+++ eon-framework/src/copy/webapp/eon/data/eon-cache/service-worker.js
@@ -1,6 +1,8 @@
 
 var CACHE = 'eon-cache';
 
+var debug = false;
+
 var filesToCache = [
   "../../eon.js"
 ];
@@ -8,56 +10,66 @@ var filesToCache = [
 // ** self = service worker
 
 /*
-* -- Install service worker
+* -- Install
 */
 self.addEventListener('install', function (e) {
-  console.log('[ServiceWorker] Install');
-
+  if(debug) { console.log('[ServiceWorker] Install'); }
   e.waitUntil(
     caches.open(CACHE).then(function (cache) {
-      console.log('[ServiceWorker] Caching app shell');
+      if(debug) { console.log('[ServiceWorker] Caching app shell'); }
       return cache.addAll(filesToCache);
     })
   );
 });
 
+/*
+* -- Activate
+*/
 self.addEventListener('activate', function (e) {
-  console.log('[ServiceWorker] Activated');
+  if(debug) { console.log('[ServiceWorker] Activated'); }
   self.clients.claim();
 });
 
-
+/*
+* -- Fetch
+*/
 self.addEventListener('fetch', function (e) {
   var req = e.request;
   var url = new URL(req.url); // ** Not supported on IE
-
   // Check same origin sources
   if (url.origin === location.origin) {
-    console.log('eon CACHE FIRST');
+    if(debug) { console.log('eon cache first'); }
     e.respondWith(cacheFirst(req));
   } else {
-    console.log('eon NET AND CACHE');
+    if(debug) { console.log('eon net and cache'); }
     e.respondWith(networkAndCache(req));
   }
 });
 
+/*
+* -- Network fallback to cache
+*/
 function cacheFirst(req) {
   return caches.open(CACHE).then(function (cache) {
     return cache.match(req).then(function (cached) {
-      console.log('eon match!', req);
       return cached || fetch(req);
     });
   })
 }
 
+/*
+* -- Network and cache
+*/
 function networkAndCache(req) {
   return caches.open(CACHE).then(function (cache) {
     try {
+      // Request fresh data
       return fetch(req).then(function (fresh) {
         cache.put(req, fresh.clone());
         return fresh;
       });
     } catch (error) {
+      // Returned previously cached
       return cache.match(req).then(function (cached) {
         return cached;
       });
