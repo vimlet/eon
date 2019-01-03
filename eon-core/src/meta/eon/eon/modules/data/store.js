380411d3819176fc94a91bb5f6a46095c6581f5a
sergiovimlet <sergio@lightware.es>
2019-01-03 16:45:14 +0100

    Removed data directory from eon-core. Removed residual files

--- eon-core/src/meta/eon/eon/modules/data/store.js
+++ eon-core/src/meta/eon/eon/modules/data/store.js
@@ -1,74 +1,71 @@
-
 eon.store = function () {
-    var el = this;
-    /* Resources representation */
-    this.data = {};
-    
-    // Create useful callbacks
-    createCallbacks();
+  var el = this;
+  /* Resources representation */
+  this.data = {};
 
-    // Import Memory Adapter
-    importAdapter();
+  // Create useful callbacks
+  createCallbacks();
 
-    /* 
-        ##########
-        Private Functions
-        ##########
-    */
-    /*
-        @function _createCallbacks
-        @description 
-    */
-    function createCallbacks() {
-        eon.createCallback("onLoaded", el, "ready");
-        eon.createCallback("onDataChanged", el);
-    }
-    /*
-        ** TO BE REMOVED
-        @function importAdapter
-        @description 
-    */
-    function importAdapter() {
-        // Import vpa memory adapter
-       eon.amd.require([eon.basePath + "/data/eon-newstore/adapters/MemoryAdapter.js"], function (adapter) {
-            // Clone adapter functions
-            cloneFunctions(adapter());
-            //
-            createDataDescriptor();
-            // Trigger user callback once VPA has been loaded
-            eon.triggerCallback("onLoaded", el, el, [el]);
-        });
-    };
-    /*
-        @function (private) _cloneFunctions
-        @description 
-    */
-    function cloneFunctions(adapter) {
-        // Clone adapter data object
-        Object.assign(el, adapter);
-        // Get BaseAdapter prototype functions
-        Object.assign(el, adapter.constructor.prototype);
-    };
-    /*
-        @function (private) _cloneFunctions
-        @description 
-    */
-    function createDataDescriptor() {
-        // Define property descriptor with custom get and set
-        Object.defineProperty(
-            el,
-            "data",
-            {
-                get: function () {
-                    return el._memory.data;
-                },
-                set: function (value) {
-                    // Update property value
-                    el._memory.data = value;
-                    // Fire data changed event
-                    eon.triggerCallback("onDataChanged", el, el, [el.data]);
-                }
-            }
-        );
-    }
+  // Import Memory Adapter
+  importAdapter();
+  
+  /* 
+      ##########
+      Private Functions
+      ##########
+  */
+  /*
+      @function _createCallbacks
+      @description 
+  */
+  function createCallbacks() {
+    eon.createCallback("onLoaded", el, "ready");
+    eon.createCallback("onDataChanged", el);
+  }
+  /*
+      ** TO BE REMOVED
+      @function importAdapter
+      @description 
+  */
+  function importAdapter() {
+    // Clone adapter functions
+    cloneFunctions(new eon.data.MemoryAdapter());
+    // Store data access
+    createDataDescriptor();
+    // Trigger user callback once VPA has been loaded
+    eon.triggerCallback("onLoaded", el, el, [el]);
+
+  };
+  /*
+      @function (private) _cloneFunctions
+      @description 
+  */
+  function cloneFunctions(adapter) {
+    // Clone adapter data object
+    Object.assign(el, adapter);
+    // Get BaseAdapter prototype functions
+    Object.assign(el, adapter.constructor.prototype);
+  };
+  /*
+      @function (private) _cloneFunctions
+      @description 
+  */
+  function createDataDescriptor() {
+    // Define property descriptor with custom get and set
+    Object.defineProperty(
+      el,
+      "data",
+      {
+        get: function () {
+          return el._memory.data;
+        },
+        set: function (value) {
+          // Update property value
+          el._memory.data = value;
+          // Fire data changed event
+          eon.triggerCallback("onDataChanged", el, el, [el.data]);
+        }
+      }
+    );
+  }
 }
