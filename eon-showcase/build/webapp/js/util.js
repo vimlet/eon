05d009fe44aadf7ae711eb047b6a9a6402b2cb6a
sergiovimlet <sergio@lightware.es>
2019-01-21 17:42:52 +0100

    Re-implemented showcase with the new viwer component

--- eon-showcase/build/webapp/js/util.js
+++ eon-showcase/build/webapp/js/util.js
@@ -53,39 +53,18 @@ function initializeShowcase(sectionsClass, pgClass) {
     // Initialize showcase
     var sections = document.querySelector(sectionsClass).content.children;
     var pg = document.querySelector(pgClass);
-    var renderingDelay = eon.util.isTouchScreen() ? 1600 : 500;
-    
+    // var renderingDelay = eon.util.isTouchScreen() ? 1600 : 500;
+
     // Set showcase content
-    if (!~[
-      ".d-button-pg", 
-      ".d-checkbox-pg", 
-      ".d-dialog-pg", 
-      ".d-drawer-pg", 
-      ".d-loader-pg",
-      ".d-loadingmask-pg"
-  ].indexOf(pgClass)) {
-      setTimeout(function () {
-        pg.onReady(function () {
-          var pgObj = {
-            head: sections[0].innerHTML,
-            body: sections[1].innerHTML,
-            js: sections[2].innerHTML,
-            css: sections[3].innerHTML
-          };
-          pg.setData(pgObj);
-        });
-      }, renderingDelay);
-    } else {
-      pg.onReady(function () {
-        var pgObj = {
-          head: sections[0].innerHTML,
-          body: sections[1].innerHTML,
-          js: sections[2].innerHTML,
-          css: sections[3].innerHTML
-        };
-        pg.setData(pgObj);
-      })
-    }
+    pg.onReady(function () {
+      var pgObj = {
+        head: sections[0].innerHTML,
+        body: sections[1].innerHTML,
+        js: sections[2].innerHTML,
+        css: sections[3].innerHTML
+      };
+      pg.setData(pgObj);
+    });
 
     // Get content height
     pg.onContentSet(function (iframe) {
@@ -93,11 +72,6 @@ function initializeShowcase(sectionsClass, pgClass) {
       eon.createCallback("onLoaded", iframe);
       // Make showcase fit its iframe content
       iframe.onLoaded(function () {
-        // Load next showcase sections
-        // eon.onReady(function(){
-          // loadNextSections(pgClass);
-        // });
-        
         var innerDoc = iframe.contentDocument || iframe.contentWindow.document;
         var body = pg._refs.resizable ? pg._refs.resizable.body : innerDoc.body;
 
@@ -168,15 +142,79 @@ function fullScreenListeners(pg) {
   });
 }
 
-function loadNextSections(pgClass) {
-  refs.view._misc.panels.containers.onLoad(function () {
-    if (pgClass == ".d-button-pg" && Object.keys(refs.view._misc.panels.containers._refs.templates).length) {
-      // alert(refs.view._misc.panels.containers._refs.templates[0]);
-      
-      refs.view._misc.panels.containers.render();
-      refs.view._misc.panels.media.render();
-      refs.view._misc.panels.menus.render();
-      refs.view._misc.panels.other.render();
-    }
-  });
+function loadNextSections() {
+  setTimeout(function () {
+    refs.view._misc.panels.containers.onLoad(function () {
+      if (Object.keys(refs.view._misc.panels.containers._refs.templates).length) {
+        // alert(refs.view._misc.panels.containers._refs.templates[0]);
+
+        refs.view._misc.panels.containers.render();
+        refs.view._misc.panels.media.render();
+        refs.view._misc.panels.menus.render();
+        refs.view._misc.panels.other.render();
+      }
+    });
+  }, 600);
+}
+/* EXAMPLE SPECIFICS */
+/* DRAWER */
+function slideInDrawer(id) {
+  var drawer = document.querySelector("#" + id);
+  drawer.show();
+}
+/* PANEL */
+function renderLazy() {
+  document.querySelector("#d-lazy-content").render();
+  // Remove place holder
+  document.querySelector("#d-lazy-content .d-place-holder").style.display = "none";
+}
+function importRemote() {
+  document.querySelector("#d-lazy-remote").importContent();
+  // Remove place holder
+  document.querySelector("#d-lazy-remote .d-place-holder").style.display = "none";
+}
+/* LOADER */
+function runLoader(l1) {
+  var l1 = document.querySelector("#d-l1");
+  l1.animate(0, 0);
+  l1.animate(1);
+}
+function runEaseProgress() {
+  var l4 = document.querySelector("#d-l4");
+  l4.animate(0, 0);
+  l4.animate(1, 2000);
+}
+eon.onReady(function () {
+  setTimeout(function () {
+    runLoader();
+    runEaseProgress()
+  }, 1000);
+});
+/* LOADINGMASK */
+function showComplete() {
+  var loadingmask = document.querySelector("#complete");
+  loadingmask.show();
+}
+/* SEARCHBAR */
+eon.onReady(function () {
+  setTimeout(function () {
+    document.querySelector(".onlyInput").onSearch(function (filters) {
+      var name = filters.name;
+      // Get colors
+      var toFilterChildren = document.querySelector(".d-searchbar-colors").children;
+      // Show filtered colors
+      for (var i = 0; i < toFilterChildren.length; i++) {
+        if (toFilterChildren[i].getAttribute("value").toLowerCase().indexOf(name.toLowerCase()) > -1) {
+          toFilterChildren[i].classList.add("d-search-item-visible");
+        } else {
+          toFilterChildren[i].classList.remove("d-search-item-visible");
+        }
+      }
+    });
+  }, 1000);
+});
+
+function searchColor() {
+  var input = document.querySelector(".onlyInput");
+  input.search(document.querySelector(".onlyInput"));
 }
