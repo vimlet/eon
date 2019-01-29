d401ceab69aa5ca83eae370f1cc1e29b25536793
sergiovimlet <sergio@lightware.es>
2019-01-29 12:38:46 +0100

    Fixing showcase theming stuff. Modified eon theme noire main color

--- eon-showcase/src/copy/webapp/js/util.js
+++ eon-showcase/src/copy/webapp/js/util.js
@@ -6,42 +6,78 @@ function setUpSearch(selector) {
   });
 }
 function setUpTheme(selector) {
-  // Search listener
-  document.querySelector(selector).onSelected(function (item) {
-    eon.theme = item.value;
-    if(item.value == "noire") {
-      document.querySelector(".tStickyClass").classList.add("to-noire");
-      document.querySelector(".tMenuBtn").classList.add("to-noire");
-      document.querySelector(".tViewContainer").classList.add("to-noire");
-      document.querySelector(".card h1").classList.add("to-noire");
-      document.querySelector(".tTreeContainer").classList.add("to-noire");
+  var themeSelector = document.querySelector(selector);
+  // Set themes selector responsive
+  setUpSelector(themeSelector);
+  // Non EON components theme switching 
+  switchThemeListener(themeSelector);
+}
 
+function setUpSelector(themeSelector) {
+  var selectorContainer = themeSelector.parentNode;
+  var selectorContainerParent = themeSelector.parentNode.parentNode;
+  //
+  window.addEventListener("resize", function () {
+    checkResponsive();
+  });
+  function checkResponsive() {
+    if (this.innerWidth >= 1301) {
+      document.body.appendChild(selectorContainer);
+    } else {
+      selectorContainerParent.appendChild(selectorContainer);
+    }
+  }
+  checkResponsive();
+}
+function switchThemeListener(themeSelector) {
+  // Get themed showcase DOM elements
+  var logo = document.querySelector(".tIcon");
+  var elms = [".tStickyClass", ".tMenuBtn", ".tViewContainer",
+    ".tTreeContainer", ".theme-selector .eon-combobox-wrapper",
+    ".theme-selector-container", ".d-searchbar-colors", ".d-gutter eon-section",
+    "eon-gutter eon-section", ".d-gutter-root .eon-gutter-section", ".d-gutter .eon-gutter-section, .d-gutter-root .eon-gutter-section"];
+  // Search listener
+  themeSelector.onSelected(function (item) {
+    eon.theme = item.value;
+    if (item.value == "noire") {
+      // Change EON logo
+      logo.style.backgroundImage = "url(../img/eon-logo-w.svg)";
+      // DOM elements theme
+      for (var i = 0; i < elms.length; i++) {
+        var elm = document.querySelector(elms[i]);
+        console.log('elm', elm, elm.style.boxShadow);
+        if(window.getComputedStyle(elm).boxShadow) {
+          elm.style.boxShadow = "0px 0px 10px #1b1b1b";
+        }
+        elm.classList.add("to-noire");
+      }
+      // Card titles
       var titles = document.querySelectorAll(".card h1");
       for (var i = 0; i < titles.length; i++) {
-        var title = titles[i];  
+        var title = titles[i];
         title.classList.add("to-noire");
       }
-      // .tStickyClass
-      // .tMenuBtn
-      // .tViewContainer
-      // .card h1
-      // eon-viewer set theme claro
-      // .tTreeContainer set background-color to #2d2d2d
     } else {
-      document.querySelector(".tStickyClass").classList.remove("to-noire");
-      document.querySelector(".tMenuBtn").classList.remove("to-noire");
-      document.querySelector(".tViewContainer").classList.remove("to-noire");
-      document.querySelector(".card h1").classList.remove("to-noire");
-      document.querySelector(".tTreeContainer").classList.remove("to-noire");
+      // Change EON logo
+      logo.style.backgroundImage = "url(../img/eon-logo.svg)";
+      // DOM elements theme
+      for (var i = 0; i < elms.length; i++) {
+        var elm = document.querySelector(elms[i]);
+       
+        if(window.getComputedStyle(elm).boxShadow) {
+          elm.style.boxShadow = "0px 0px 10px #d8d8d8";
+        }
+        elm.classList.remove("to-noire");
+      }
+      // Card titles
       var titles = document.querySelectorAll(".card h1");
       for (var i = 0; i < titles.length; i++) {
-        var title = titles[i];  
+        var title = titles[i];
         title.classList.remove("to-noire");
       }
     }
   });
 }
-
 function loadEonExamples() {
   var anchor, activePanel, groupId;
   // Configure tree
@@ -51,10 +87,10 @@ function loadEonExamples() {
   var rootNodes = [].slice.call(refs.tree._refs.tree.children);
   // Load vComet element example
   refs.tree.onNodeSelected(function (node) {
-    if(!refs.cancelTreeSelection) {
+    if (!refs.cancelTreeSelection) {
       // Go to group file
       groupId = node._refs.parentNode.tagName == "EON-TREENODE" ? node._refs.parentNode.id : node.id;
-  
+
       refs.view.swapToPanel(groupId);
       // Get active panel
       activePanel = refs.view.getActivePanel();
@@ -83,7 +119,7 @@ function loadEonExamples() {
   });
   // Initialize forms
   eon.onReady(function () {
-    refs.view.onReady(function(){
+    refs.view.onReady(function () {
       refs.view._misc.activePanel.onLoad(function () {
         // Set up scroll anchor activation
         anchorScrolling(this);
@@ -113,7 +149,7 @@ function initializeShowcase(sectionsClass, pgClass) {
     var sections = document.querySelector(sectionsClass).content.children;
     var pg = document.querySelector(pgClass);
     // var renderingDelay = eon.util.isTouchScreen() ? 1600 : 500;
-    
+
     // Set showcase content
     pg.onReady(function () {
       var pgObj = {
@@ -204,7 +240,7 @@ function fullScreenListeners(pg) {
 }
 
 function loadNextSections() {
-  
+
   setTimeout(function () {
     eon.onReady(function () {
       refs.view._misc.panels.containers.onLoad(function () {
@@ -225,7 +261,7 @@ function loadNextSections() {
           // Fix Togglemenu displaying
         }
       });
-
+      refs.view._misc.panels.forms.content.parentNode.scrollTop = 0;
       refs.mask.hide();
     });
   }, 600);
@@ -240,9 +276,9 @@ function anchorScrolling(panel) {
       var name;
       // On anchor activated
       for (var i = 0; i < anchors.length; i++) {
-        anchors[i].onReached(function(){
+        anchors[i].onReached(function () {
           name = this.state;
-          refs.tree.onReady(function(){
+          refs.tree.onReady(function () {
             //
             refs.cancelTreeSelection = true;
             refs.tree.selectNode(refs.tree.nodes[panel.name + "/" + name]);
