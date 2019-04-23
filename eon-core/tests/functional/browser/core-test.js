c90482248e01ac7130353079af0025f1d2b89305
sergiovimlet <sergio@lightware.es>
2019-04-23 16:13:20 +0200

    Added more testing platforms to eon tests

--- eon-core/tests/functional/browser/core-test.js
+++ eon-core/tests/functional/browser/core-test.js
@@ -2,62 +2,70 @@ const { suite, test } = intern.getInterface("tdd");
 const { assert } = intern.getPlugin("chai");
 
 suite("eon-core", () => {
-    test("Imports", ({ remote }) => {
-        return remote.get(intern.config.remoteUrl + "/index.html").sleep(1000).executeAsync(function (callback) {
-            importsTest(callback);
-        }).then(function (value) {
-            assert.strictEqual(value, true);
-        });
-
+  test("Imports", ({ remote }) => {
+    remote.setExecuteAsyncTimeout(20000);
+    return remote.get(intern.config.remoteUrl + "/index.html").sleep(1000).executeAsync(function (callback) {
+      importsTest(callback);
+    }).then(function (value) {
+      assert.strictEqual(value, true);
     });
-    test("Data diffing instance", ({ remote }) => {
-        return remote.get(intern.config.remoteUrl + "/index.html").sleep(1000).executeAsync(function (callback) {
-          dataDiffingInstanceTest(callback);
-        }).then(function (value) {
-          assert.strictEqual(value, true);
-        });
-      });
-      test("Data diffing commit", ({ remote }) => {
-        return remote.get(intern.config.remoteUrl + "/index.html").sleep(1000).executeAsync(function (callback) {
-          dataDiffingCommitTest(callback);
-        }).then(function (value) {
-          assert.strictEqual(value, true);
-        });
-      });
-      test("Resize", ({ remote }) => {
-        return remote.get(intern.config.remoteUrl + "/index.html").sleep(1000).executeAsync(function (callback) {
-          resizeTest(callback);
-        }).then(function (value) {
-          assert.strictEqual(value, true);
-        });
-      });
-      test("Template references", ({ remote }) => {
-        return remote.get(intern.config.remoteUrl + "/index.html").sleep(1000).executeAsync(function (callback) {
-          templateReferencesTest(callback);
-        }).then(function (value) {
-          assert.strictEqual(value, true);
-        });
-      });
-      test("Binding", ({ remote }) => {
-        return remote.get(intern.config.remoteUrl + "/index.html").sleep(1000).executeAsync(function (callback) {
-          bindingTest(callback);
-        }).then(function (value) {
-          assert.strictEqual(value, true);
-        });
-      });
-      test("Data & Locale changes", ({ remote }) => {
-        return remote.get(intern.config.remoteUrl + "/index.html").sleep(1000).executeAsync(function (callback) {
-          dataChangedTest(callback);
-        }).then(function (value) {
-          assert.strictEqual(value, true);
-        });
-      });
-      test("Property changes", ({ remote }) => {
-        return remote.get(intern.config.remoteUrl + "/index.html").sleep(1000).executeAsync(function (callback) {
-            propertyChangedTest(callback);
-        }).then(function (value) {
-            assert.strictEqual(value, true);
-        });
 
+  });
+  test("Data diffing instance", ({ remote }) => {
+    remote.setExecuteAsyncTimeout(20000);
+    return remote.get(intern.config.remoteUrl + "/index.html").sleep(1000).executeAsync(function (callback) {
+      dataDiffingInstanceTest(callback);
+    }).then(function (value) {
+      assert.strictEqual(value, true);
     });
+  });
+  test("Data diffing commit", ({ remote }) => {
+    remote.setExecuteAsyncTimeout(20000);
+    return remote.get(intern.config.remoteUrl + "/index.html").sleep(1000).executeAsync(function (callback) {
+      dataDiffingCommitTest(callback);
+    }).then(function (value) {
+      assert.strictEqual(value, true);
+    });
+  });
+  test("Resize", ({ remote }) => {
+    remote.setExecuteAsyncTimeout(20000);
+    return remote.get(intern.config.remoteUrl + "/index.html").sleep(1000).executeAsync(function (callback) {
+      resizeTest(callback);
+    }).then(function (value) {
+      assert.strictEqual(value, true);
+    });
+  });
+  test("Template references", ({ remote }) => {
+    remote.setExecuteAsyncTimeout(20000);
+    return remote.get(intern.config.remoteUrl + "/index.html").sleep(1000).executeAsync(function (callback) {
+      templateReferencesTest(callback);
+    }).then(function (value) {
+      assert.strictEqual(value, true);
+    });
+  });
+  test("Binding", ({ remote }) => {
+    remote.setExecuteAsyncTimeout(20000);
+    return remote.get(intern.config.remoteUrl + "/index.html").sleep(1000).executeAsync(function (callback) {
+      bindingTest(callback);
+    }).then(function (value) {
+      assert.strictEqual(value, true);
+    });
+  });
+  test("Data & Locale changes", ({ remote }) => {
+    remote.setExecuteAsyncTimeout(20000);
+    return remote.get(intern.config.remoteUrl + "/index.html").sleep(1000).executeAsync(function (callback) {
+      dataChangedTest(callback);
+    }).then(function (value) {
+      assert.strictEqual(value, true);
+    });
+  });
+  test("Property changes", ({ remote }) => {
+    remote.setExecuteAsyncTimeout(20000);
+    return remote.get(intern.config.remoteUrl + "/index.html").sleep(1000).executeAsync(function (callback) {
+      propertyChangedTest(callback);
+    }).then(function (value) {
+      assert.strictEqual(value, true);
+    });
+
+  });
 });
