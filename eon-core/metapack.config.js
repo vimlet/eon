dc697261f884e8cc30785cc3e0ee5c64a2fda31c
lethevimlet <lethevimlet@mail.com>
2019-07-21 21:44:02 +0200

    WIP metapack config

--- eon-core/metapack.config.js
+++ eon-core/metapack.config.js
@@ -8,35 +8,15 @@ module.exports = {
         return entry;
       },
       "input": {
-        "parse": true,
-        "src/meta/**.vmt": true
+        "src/meta/**.vmt": {
+          "parse": true
+        }
       }
     },
-    //   "**": [
-    //     {
-    //       "clean": false,
-    //       "input": {
-    //         "less/**.less": {
-    //           "use": async function (entry) {
-    //             try {
-    //               entry.fileName = entry.fileName.replace(".less", ".css");
-    //               entry.content = (await require("less").render(entry.content.toString("utf8"), {
-    //                 filename: require("path").resolve(entry.file),
-    //               })).css;
-    //             } catch (error) {
-    //               console.log(error);
-    //             }
-    //             return entry;
-    //           }
-    //         }
-    //       }
-    //     },
-    //     {
-    //       "clean": false,
-    //       "input": {
-    //         "copy/**": true
-    //       }
-    //     }
-    //   ]
+    "build/**": {
+      "input": {
+        "src/copy/**": true
+      }
+    }
   }
 };
