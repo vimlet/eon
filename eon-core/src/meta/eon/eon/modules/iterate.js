2b936d0d018e671c4dc798118c934e8ade140b3b
richardvimletm <richardillesk@gmail.com>
2020-08-21 17:00:13 +0200

    Added option callback to iterate

--- eon-core/src/meta/eon/eon/modules/iterate.js
+++ eon-core/src/meta/eon/eon/modules/iterate.js
@@ -5,45 +5,49 @@
 @param {Number} batch
 @param {Number} delay
 */
-eon.iterate = function (iterable, batch, delay) {
-    batch = batch || iterable.length;
+eon.iterate = function (iterable, batch, delay, callback) {
+  batch = batch || iterable.length;
 
-    if (batch == 0) {
-      batch = iterable.length;
-    }
+  if (batch == 0) {
+    batch = iterable.length;
+  }
 
-    var current = 0;
-    var nextCount = 0;
-    var doneCount = 0;
+  var current = 0;
+  var nextCount = 0;
+  var doneCount = 0;
 
-    var done = function () {
-      doneCount++;
-      if (doneCount == batch) {
-        if (delay != null) {
-          setTimeout(function () {
-            nextCount = 0;
-            doneCount = 0;
-            next();
-          }, delay);
-        } else {
+  var done = function () {
+    doneCount++;
+    if (doneCount == batch) {
+      if (delay != null) {
+        setTimeout(function () {
           nextCount = 0;
           doneCount = 0;
           next();
-        }
+        }, delay);
+      } else {
+        nextCount = 0;
+        doneCount = 0;
+        next();
       }
-    };
-
-    var next = function () {
-      if (current < iterable.length) {
-        if (nextCount < batch) {
-          var e = iterable[current];
-          current++;
-          nextCount++;
-          e.run(done);
-          next();
-        }
-      }
-    };
-
-    next();
+    }
   };
+
+  var next = function () {
+    if (current < iterable.length) {
+      if (nextCount < batch) {
+        var e = iterable[current];
+        current++;
+        nextCount++;
+        e.run(done);
+        next();
+      }
+    }else{
+      if(callback){
+        callback();
+      }
+    }
+  };
+
+  next();
+};
